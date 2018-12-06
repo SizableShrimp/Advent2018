ee007783af2f344bb15ab8776950ac0a5ad85c07
SizableShrimp <jcharles08@me.com>
2018-12-05 21:05:02 -0600

    Set up Day 6

---
+++ src/main/java/me/sizableshrimp/adventofcode/days/Day6.java
@@ -0,0 +1,15 @@
+package me.sizableshrimp.adventofcode.days;
+
+import me.sizableshrimp.adventofcode.Day;
+
+public class Day6 extends Day {
+    @Override
+    public Object part1() {
+        return null;
+    }
+
+    @Override
+    public Object part2() {
+        return null;
+    }
+}
