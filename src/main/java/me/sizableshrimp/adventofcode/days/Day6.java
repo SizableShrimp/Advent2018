86a46868ac2d15708aa70fd7cc83c61e2d5f096d
SizableShrimp <jcharles08@me.com>
2018-12-06 20:26:54 -0600

    Update Day 6 Uses Lombok Dynamically sizes grid array

--- src/main/java/me/sizableshrimp/adventofcode/days/Day6.java
+++ src/main/java/me/sizableshrimp/adventofcode/days/Day6.java
@@ -1,50 +1,65 @@
 package me.sizableshrimp.adventofcode.days;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import me.sizableshrimp.adventofcode.Day;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Day6 extends Day {
+    @AllArgsConstructor @EqualsAndHashCode
     private class Coordinate {
         int x,y;
-
-        Coordinate(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
 
         int distance(int x2, int y2) {
             return Math.abs(x - x2) + Math.abs(y - y2);
         }
-
-        @Override
-        public boolean equals(Object o) {
-            if (!(o instanceof Coordinate)) return false;
-            Coordinate coordinate = (Coordinate) o;
-            return coordinate.x == this.x && coordinate.y == this.y;
-        }
     }
 
     private Map<Integer, Coordinate> coordinates = new HashMap<>();
-    private int[][] grid = new int[500][500];
+    private int[][] grid;
 
-    @Override
-    protected Object part1() {
+    public Day6() {
+        super();
         int id = 1;
         for (String line : lines) {
             String[] split = line.split(", ");
             coordinates.put(id, new Coordinate(Integer.parseInt(split[0]), Integer.parseInt(split[1])));
             id++;
         }
-        int area = findBiggestArea();
-//        for (int[] innerArray : grid) {
-//            System.out.println(Arrays.toString(innerArray));
-//        }
-        return area;
+        int maxX = coordinates.values()
+                .stream()
+                .mapToInt(c -> c.x)
+                .max().orElse(500);
+        int maxY = coordinates.values()
+                .stream()
+                .mapToInt(c -> c.y)
+                .max().orElse(500);
+        grid = new int[maxX+1][maxY+1];
+    }
+
+    @Override
+    protected Object part1() {
+        calculateClosestDistances();
+
+        Map<Integer, Integer> sizes = new HashMap<>();
+        Set<Integer> infiniteIds = new HashSet<>(); //a list of the ids that touch the edge, meaning they are infinite
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                int id = grid[i][j];
+                if (id == 0 || id == -1 || infiniteIds.contains(id)) continue;
+                if (i == 0 || j == 0 || i == grid.length - 1 || j == grid[i].length - 1) {
+                    infiniteIds.add(id);
+                    sizes.remove(id);
+                }
+                sizes.merge(id, 1, Integer::sum);
+            }
+        }
+        return Collections.max(sizes.values());
     }
 
     @Override
@@ -52,7 +67,7 @@ public class Day6 extends Day {
         int regionSize = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                final Coordinate coordinate = new Coordinate(j, i);
+                final Coordinate coordinate = new Coordinate(i, j);
                 int sum = coordinates.values()
                         .stream()
                         .mapToInt(c -> c.distance(coordinate.x, coordinate.y))
@@ -63,39 +78,15 @@ public class Day6 extends Day {
         return regionSize;
     }
 
-    private int findBiggestArea() {
-        calculateClosestDistances();
-
-        Map<Integer, Integer> sizes = new HashMap<>();
-        List<Integer> infiniteIds = new ArrayList<>(); //a list of the ids that touch the edge, meaning they are infinite
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[i].length; j++) {
-                int id = grid[j][i];
-                if (id == 0 || id == -1) continue;
-                if (i == 0 || j == 0 || i == grid.length - 1 || j == grid[i].length - 1) {
-                    infiniteIds.add(id);
-                    sizes.remove(id);
-                }
-                if (infiniteIds.contains(id)) continue;
-                sizes.merge(id, 1, Integer::sum);
-            }
-        }
-        //System.out.println(Arrays.toString(sizes.values().toArray()));
-        return Collections.max(sizes.values());
-    }
-
     private void calculateClosestDistances() {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (coordinates.values().contains(new Coordinate(j, i))) {
-                    grid[j][i] = getKey(new Coordinate(j, i));
-                    continue;
-                }
+                if (isCoordinate(new Coordinate(i, j))) continue;
                 int closestId = 0; //the id of the closest coordinate
                 int closestValue = -1; //the distance between the closest coordinate
                 boolean mayBeEquidistant = false;
                 for (Map.Entry<Integer, Coordinate> entry : coordinates.entrySet()) {
-                    int distance = entry.getValue().distance(j, i);
+                    int distance = entry.getValue().distance(i, j);
                     if (distance < closestValue || closestValue == -1) {
                         closestId = entry.getKey();
                         closestValue = distance;
@@ -104,15 +95,20 @@ public class Day6 extends Day {
                         mayBeEquidistant = true;
                     }
                 }
-                if (!mayBeEquidistant) grid[j][i] = closestId;
+                if (!mayBeEquidistant) grid[i][j] = closestId;
             }
         }
     }
 
-    private int getKey(Coordinate coordinate) {
-        for (Map.Entry<Integer, Coordinate> entry : coordinates.entrySet()) {
-            if (entry.getValue().equals(coordinate)) return entry.getKey();
+    private boolean isCoordinate(Coordinate coordinate) {
+        if (coordinates.containsValue(coordinate)) {
+            int id = -1;
+            for (Map.Entry<Integer, Coordinate> entry : coordinates.entrySet()) {
+                if (entry.getValue().equals(coordinate)) id = entry.getKey();
+            }
+            grid[coordinate.x][coordinate.y] = id;
+            return true;
         }
-        return 0;
+        return false;
     }
 }
