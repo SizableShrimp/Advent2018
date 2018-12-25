4bc99931a8818c61eea46929d7d9d6109ccd344c
SizableShrimp <29855886+SizableShrimp@users.noreply.github.com>
2018-12-24 22:17:18 -0600

    Update Day 14 (the upcoming changes are all stuff I have already done just not committed)

--- src/main/java/me/sizableshrimp/adventofcode/days/Day14.java
+++ src/main/java/me/sizableshrimp/adventofcode/days/Day14.java
@@ -1,39 +1,27 @@
 package me.sizableshrimp.adventofcode.days;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Setter;
 import me.sizableshrimp.adventofcode.Day;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.stream.Collectors;
 
 public class Day14 extends Day {
     @AllArgsConstructor
     private class Elf {
+        @Setter
         private int currentIndex; //the index in the list of the current score
-        @Getter
-        private int currentScore;
 
         private void pickNewScore() {
-            ListIterator<Integer> iter = scoreboard.listIterator(currentIndex);
-            int nextIndex = -1;
-            for (int i = 0; i <= 1 + currentScore; i++) {
-                if (!iter.hasNext()) iter = scoreboard.listIterator();
-                nextIndex = iter.nextIndex();
-                iter.next();
-                //System.out.println(nextIndex);
-            }
-            //int nextIndex = (1 + currentScore) % scoreboard.size();
-            setCurrentIndex(nextIndex);
-            //System.out.println("Elf picked "+currentScore+" at index "+ currentIndex);
+            currentIndex = (currentIndex + 1 + getCurrentScore()) % scoreboard.size();
+            //System.out.println("Elf picked "+getCurrentScore()+" at index "+ currentIndex);
         }
 
-        private void setCurrentIndex(int index) {
-            currentIndex = index;
-            currentScore = scoreboard.get(index);
+        private int getCurrentScore() {
+            return scoreboard.get(currentIndex);
         }
     }
 
@@ -46,20 +34,13 @@ public class Day14 extends Day {
     protected Object part1() {
         scoreboard.add(3);
         scoreboard.add(7);
-        firstElf = new Elf(0, 3);
-        secondElf = new Elf(1, 7);
+        firstElf = new Elf(0);
+        secondElf = new Elf(1);
 
-        for (int i = 0; i < INPUT + 10; i++) {
-            int sum = firstElf.getCurrentScore() + secondElf.getCurrentScore();
-            List<Integer> digits = parseDigits(sum);
-            scoreboard.addAll(digits);
-            firstElf.pickNewScore();
-            secondElf.pickNewScore();
-        }
-        System.out.println(scoreboard);
-        int startingIndex = INPUT;
+        makeRecipe(INPUT + 10);
+
         StringBuilder result = new StringBuilder();
-        for (int i = startingIndex; i < startingIndex + 10; i++) {
+        for (int i = INPUT; i < INPUT + 10; i++) {
             result.append(scoreboard.get(i));
         }
         return result.toString();
@@ -69,18 +50,51 @@ public class Day14 extends Day {
     protected Object part2() {
         List<Integer> digits = parseDigits(INPUT);
 
-        for (int i = 0; i < scoreboard.size(); i++) {
-            int score = scoreboard.get(i);
-            if (digits.get(0) != score) continue;
-            boolean valid = true;
-            for (int j = 1; j < digits.size(); j++) {
-                if (!digits.get(j).equals(scoreboard.get(i + j))) {
-                    valid = false;
-                } else System.out.println(digits.get(j));
-            }
-            if (valid) return i+1;
+        return findFirstIndex(digits);
+    }
+
+    private void makeRecipe(int amount) {
+        for (int i = 0; i < amount; i++) {
+            makeRecipe();
         }
-        return null;
+    }
+
+    private void makeRecipe() {
+        int sum = firstElf.getCurrentScore() + secondElf.getCurrentScore();
+        List<Integer> digits = parseDigits(sum);
+        scoreboard.addAll(digits);
+        firstElf.pickNewScore();
+        secondElf.pickNewScore();
+    }
+
+    private int findFirstIndex(List<Integer> digits) {
+        Integer index = null;
+        int startIndex = 0;
+
+        while (index == null) {
+            for (int i = startIndex; i < scoreboard.size(); i++) {
+                if (index != null) break; //don't want to reassign value after it appears once
+                int score = scoreboard.get(i);
+                if (digits.get(0) != score) continue;
+                boolean valid = true;
+                for (int j = 1; j < digits.size(); j++) {
+                    if (i + j >= scoreboard.size()) {
+                        valid = false;
+                        break;
+                    }
+                    if (!digits.get(j).equals(scoreboard.get(i + j))) {
+                        valid = false;
+                    }
+                }
+                if (valid) index = i;
+            }
+            if (index == null) {
+                //start where list ended and account for rare case where the list of digits may have been cut off
+                startIndex = scoreboard.size() - digits.size();
+                makeRecipe(1_000_000);
+            }
+        }
+        return index;
     }
 
     private List<Integer> parseDigits(Integer number) {
